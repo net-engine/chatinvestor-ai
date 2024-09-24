6f5774c260143b3169983260380d8c34b80e08fa
netengine-jt <jiatao@netengine.com.au>
2024-09-24 14:48:14 +1000

    Initial commit

---
+++ test/chatinvestor.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Chatinvestor from '../lib/chatinvestor-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/chatinvestor-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Chatinvestor.ChatinvestorStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
