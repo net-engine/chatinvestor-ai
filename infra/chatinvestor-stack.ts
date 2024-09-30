33cca87e9dcb0f97e8fff783183648917661e511
netengine-jt <jiatao@netengine.com.au>
2024-09-30 13:05:56 +1000

    feat: Restructure the dir

--- infra/chatinvestor-stack.ts
+++ infra/chatinvestor-stack.ts
@@ -2,13 +2,15 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
 import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
+import * as path from "path";
+
 export class ChatinvestorStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const chatLambda = new Function(this, "chatLambdaFunction", {
       runtime: Runtime.NODEJS_18_X,
-      code: Code.fromAsset("api"),
+      code: Code.fromAsset(path.join(__dirname, "..", "api")),
       handler: "chat.handler",
     });
 
--- infra/chatinvestor.ts
+++ infra/chatinvestor.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { ChatinvestorStack } from "../lib/chatinvestor-stack";
+import { ChatinvestorStack } from "./chatinvestor-stack";
 
 const app = new cdk.App();
 new ChatinvestorStack(app, "ChatinvestorStack", {
